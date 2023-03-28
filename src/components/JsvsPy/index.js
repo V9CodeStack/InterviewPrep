fa922b76d347b33458a75c2c78ee0e5b4a031a1e
V9CodeStack <v9codestack@gmail.com>
2023-03-28 20:27:29 +0530

    first commit

--- src/components/JsvsPy/index.js
+++ src/components/JsvsPy/index.js
@@ -171,6 +171,277 @@ for (j in charCount) {
 `,
     pythonCode: ``,
   },
+  /* **** question 8 **** */
+  {
+    id: 8,
+    question: `8. Implement a function that returns the Nth Fibonacci number.?`,
+    javaScriptCode: `/** JavaScript **/
+let n = 9;
+
+function fibonacci(n) {
+  if (n <= 1) {
+    return n;
+  } else {
+    return fibonacci(n - 2) + fibonacci(n - 1);
+  }
+}
+console.log(fibonacci(n)); // Output: 34`,
+    pythonCode: ` /** Python **/
+n = 9
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-2) + fibonacci(n-1)
+
+print(fibonacci(n)) // Output: 34
+`,
+  },
+  /* **** question 9 **** */
+  {
+    id: 9,
+    question: `9. Implement a logic that gives list of prime numbers ?`,
+    javaScriptCode: `/** JavaScript **/
+let n = 10;
+
+for (let i = 2; i <= n; i++) {
+  let sum = 0;
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+      sum += 1;
+      break;
+    }
+  }
+  if (sum === 0) {
+    console.log(i);
+  }
+}
+// Output: 2 3 5 7
+`,
+    pythonCode: ``,
+  },
+  /* **** question 10 **** */
+  {
+    id: 10,
+    question: `10. Implement a logic that checks whether a given number is a prime number or not.?`,
+    javaScriptCode: `/** JavaScript **/
+let n = 10;
+
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  } else {
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) {
+        return false;
+        break;
+      }
+    }
+    return true;
+  }
+}
+console.log(isPrime(n)); // Output: false`,
+    pythonCode: ``,
+  },
+  /* **** question 11 **** */
+  {
+    id: 11,
+    question: `11. Implement a function that takes a sorted array of integers and returns the index of a target number, or -1 if it's not found (using binary search). ?`,
+    javaScriptCode: `/** JavaScript **/
+const arr = [1, 3, 5, 7, 9, 11, 13];
+const target = 9;
+
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+console.log(binarySearch(arr, target)); // Output: 4
+
+// Another Method
+
+let result = arr.findIndex((eachValue) => eachValue === target);
+console.log(result); // Output: 4`,
+    pythonCode: ``,
+  },
+  /* **** question 12 **** */
+  {
+    id: 12,
+    question: `12. Highest repeating number and occurrence ?`,
+    javaScriptCode: `/** JavaScript **/
+
+const input = [1, 1, 2, 4, 1, 5, 2, 6];
+
+const countsObj = {};
+let highestCount = 0;
+let highestNumber = null;
+
+for (let i = 0; i < input.length; i++) {
+  const number = input[i];
+  countsObj[number] = countsObj[number] ? countsObj[number] + 1 : 1;
+  if (countsObj[number] > highestCount) {
+    highestCount = countsObj[number];
+    highestNumber = number;
+  }
+}
+console.log(countsObj); // { '1': 3, '2': 2, '4': 1, '5': 1, '6': 1 }
+
+// Output: highest repeatingnumber is 1, occurence is 3`,
+    pythonCode: ``,
+  },
+  /* **** question 13 **** */
+  {
+    id: 13,
+    question: `13. Write a function that takes an array of integers and returns a new array with all the duplicates removed. ?`,
+    javaScriptCode: `/** JavaScript **/
+const arr = [1, 2, 3, 1, 2, 4, 5, 4];
+
+let uniqueArry = [];
+
+for (let i = 0; i < arr.length; i++) {
+  if (uniqueArry.indexOf(arr[i]) === -1) {
+    uniqueArry.push(arr[i]);
+  }
+}
+console.log(uniqueArry);  // Output: [ 1, 2, 3, 4, 5 ]
+`,
+    pythonCode: ``,
+  },
+  /* **** question 14 **** */
+  {
+    id: 14,
+    question: `14. Implement a function that takes an array of objects and sorts them by a given property.?`,
+    javaScriptCode: `/** JavaScript **/
+const people = [
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 30 },
+  { name: "Charlie", age: 20 },
+];
+
+function sortByProperty(arr, prop) {
+  return arr.sort((a, b) => a[prop] - b[prop]);
+}
+
+const sortByAge = sortByProperty(people, "age");
+console.log(sortByAge);
+
+//Output:
+[
+  { name: "Charlie", age: 20 },
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 30 },
+];`,
+    pythonCode: `/** Python **/
+people = [
+    {"name": "Alice", "age": 25},
+    {"name": "Bob", "age": 30},
+    {"name": "Charlie", "age": 20},
+]
+
+def sort_by_property(arr, prop):
+    return sorted(arr, key=lambda x: x[prop])
+
+sortByAge = sort_by_property(people, "age")
+print(sortByAge)
+
+// Output: 
+[
+{'name': 'Charlie', 'age': 20}, 
+ {'name': 'Alice', 'age': 25},
+ {'name': 'Bob', 'age': 30}
+ ]
+`,
+  },
+  /* **** question 15 **** */
+  {
+    id: 15,
+    question: `15. Write a function that takes a string and returns the number of vowels it contains.?`,
+    javaScriptCode: `/** JavaScript **/
+const str = "Hello World";
+
+function countVowels(str) {
+  const vowles = ["a", "e", "i", "o"];
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (vowles.includes(str[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countVowels(str)); //Output: 3`,
+    pythonCode: `/** Python **/
+str = "Hello World"
+
+def count_vowles(str):
+    vowles = ["a", "e", "i", "o"]
+    count = 0
+    for char in str:
+        if char in vowles:
+            count += 1
+    return count
+
+print(count_vowles(str)) // Output: 3`,
+  },
+  /* **** question 16 **** */
+  {
+    id: 16,
+    question: `16. Implement a function that takes a number and returns its factorial. ?`,
+    javaScriptCode: `/** JavaScript **/
+let n = 4;
+
+function factorial(n) {
+  if (n <= 1) {
+    return n;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+console.log(factorial(n)); //Output: 24
+`,
+    pythonCode: ``,
+  },
+  /* **** question 17 **** */
+  {
+    id: 17,
+    question: `17. Implement a function that takes an array of integers and returns the largest product of any two integers.?`,
+    javaScriptCode: `/** JavaScript **/
+const arr = [4, 1, 0, 9, 7, 2, 4, 1, 6];
+
+let largestProduct = 0;
+for (let i = 0; i < arr.length; i++) {
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[i] * arr[j] > largestProduct) {
+      largestProduct = arr[i] * arr[j];
+    }
+  }
+}
+console.log(largestProduct); //Output: 63`,
+    pythonCode: `/** Python **/
+arr = [4, 1, 0, 9, 7, 2, 4, 1, 6]
+
+largestProduct = 0
+for i in range(len(arr)):
+    for j in range(i+1, len(arr)):
+        if arr[i] * arr[j] > largestProduct:
+            largestProduct = arr[i] * arr[j]
+            
+print(largestProduct) // Output: 63
+`,
+  },
 ];
 
 const ObjBluePrint = {
